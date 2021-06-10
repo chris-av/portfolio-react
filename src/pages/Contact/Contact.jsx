f994187864496e7457970449c17bd466d7234cd6
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-06-09 21:36:43 -0700

    implemented contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import style from './contact.module.scss';
 
@@ -9,20 +9,30 @@ const Contact = () => {
 	const [ message, setMessage ] = useState('');
 	const [ status, setStatus ] = useState(false);
 
-	const handleSubmit = (event) => {
-		// event.preventDefault();
-		console.log({ name, email, message });
-		setStatus(true);
+	const encode = data => {
+		return Object.keys(data).map(key => {
+			return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+		}).join('&');
 	}
 
-	useEffect(() => {
-		if (window.location.search.includes('success=true')) {
-			setStatus(true);
-		}
-	}, []);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setStatus(true);
+		console.log(encode({ name, email, message }));
+		console.log('sending fetch!');
+		fetch('/', {
+			method: 'POST',
+			body: encode({ 'form-name': 'contact', name, email, message }),
+			headers: {
+				'Accept': 'application/json',
+				"Content-Type": "application/x-www-form-urlencoded"
+			}
+		})
+			.then(res => console.log(res))
+			.catch(err => console.log(err))
+	}
 
 	const handleChange = (event) => {
-		// console.log(event.target.name);
 		if (event.target.name === 'name') setName(event.target.value);
 		if (event.target.name === 'email') setEmail(event.target.value);
 		if (event.target.name === 'message') setMessage(event.target.value);
@@ -39,28 +49,29 @@ const Contact = () => {
 				</div>
 
 				{ status === false ? (
-					<form name="contact" method="POST" data-netlify="true" action="/contact/?success=true">
+					<form name="contact" method="POST" data-netlify="true" action="/contact?success=true" onSubmit={handleSubmit}>
 						<div className={style['form-group']}>
 							<label>Enter your name</label>
-							<input placeholder="Enter name" onChange={handleChange} id="name" name="name" />
+							<input placeholder="Enter name" onChange={handleChange} id="name" name="name" value={name} />
 						</div>
 						
 						<div className={style['form-group']}>
 							<label>Email address</label>
-							<input type="email" placeholder="Enter email" onChange={handleChange} id="email" name="email" />
+							<input type="email" placeholder="Enter email" onChange={handleChange} id="email" name="email" value={email} />
 						</div>
 
 						<div className={style['form-group']}>
 							<label>Your inquiry</label>
-							<textarea placeholder="What can I assist you with?" onChange={handleChange} id="message" name="message" />
+							<textarea placeholder="What can I assist you with?" onChange={handleChange} id="message" name="message" value={message} />
 						</div>
 						
-						<button type="submit" className={style['submit']} onClick={handleSubmit}>Submit</button>
+						<button type="submit" className={style['submit']}>Submit</button>
 						
 					</form>	
 				) : (
 					<div className={style['complete-container']}>
-						<h2>Complete!</h2>
+						<h2>Thank you!</h2>
+						<p>Let me review your request and I'll try to get back to you as soon as possible.</p>
 					</div>
 				) }
 				
