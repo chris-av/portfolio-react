1b8292647330fbc2ed784b93fc2dc5c114ce6e9d
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-06-09 22:54:06 -0700

    modified socials

--- src/socials.js
+++ src/socials.js
@@ -1,3 +1,3 @@
 export const linkedin = 'https://www.linkedin.com/in/christian-a-valenzuela/';
-export const github = 'https://github.com/im2wddrf';
+export const github = 'https://github.com/chris-av';
 export const instagram = 'https://www.instagram.com/chrisandre95';
