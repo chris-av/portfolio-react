b76798391c648f29111477a90ee90297e9c1c41b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-04-24 01:17:26 -0700

    replace boring list with animated cards

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,109 +1,48 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { animated, useSpring } from 'react-spring';
-import useMeasure from 'react-use-measure';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-import styles from './services.module.scss';
-import * as Icons from '../Icons/Icons';
+import s from './services.module.scss';
+
+import AnimatedCard from '../AnimatedCard/AnimatedCard';
 
 
-const toggle = {
-  width: '1em',
-  height: '1em',
-  marginRight: 10,
-  cursor: 'pointer',
-  verticalAlign: 'middle',
-}
+const Services = () => { return (
+    <div className={s['container']}>
 
-const usePrevious = (value) => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-}
+      <div className={s["cards"]}>
 
-const Tree = React.memo(({ children, name, style, defaultOpen = false }) => {
-  const [ isOpen, setOpen ] = useState(defaultOpen);
-  const previous = usePrevious(isOpen);
-  const [ ref, { height: viewHeight } ] = useMeasure();
-  const { height, opacity, y } = useSpring({
-    from: { height: 0, opacity: 0, y: 0 },
-    to: {
-      height: isOpen ? viewHeight: 0,
-      opacity: isOpen ? 1 : 0,
-      y: isOpen ? 0 : 20
-    }
-  })
+        <AnimatedCard front="Landing Pages">
+          <p>I can help you build out landing pages for your products!</p>
+        </AnimatedCard>
 
-  const Icon = Icons[ `${ children ? (isOpen ? 'Minus' : 'Plus') : 'Close'}SquareO` ];
+        <AnimatedCard front="Portfolios">
+          <p>Are you an artist? A lawyer? A photographer? If you need help showcasing your work hit me up!</p>
+          <p>I can create the perfect website to beautifully present your hard work and talents!</p>
+        </AnimatedCard>
 
-  return (
-    <div className={styles['frame']}>
-      <Icon style={{ ...toggle, opacity: children ? 1 : 0.3 }} onClick={() => setOpen(!isOpen)} />
-      <span className={styles['title']} style={style}>{ name }</span>
-      <animated.div
-        className={styles['content']}
-        style={{
-          opacity,
-          height: isOpen && previous === isOpen ? 'auto' : height
-        }}
-      >
-        <div 
-          ref={ref} 
-          style={{ y }} 
-          children={children} 
-        />
-      </animated.div>
-    </div>
-  );
-});
+        <AnimatedCard front="E-commerce">
+          <p>I can connect your payment solution to your personal website, whether its Wordpress, Squarespace or other website platform!</p>
+        </AnimatedCard>
 
-const Services = () => {
-  return (
-    <div className={styles['container']}>
-      <div name="" defaultOpen>
-        <Tree name="Blogging">
-          <div>Word Press</div>
-          <div>Custom solutions</div>
-        </Tree>
-        <Tree name="Landing Pages">
-          <div>Single Page Applications</div>
-          <div>Mailing lists</div>
-        </Tree>
-        <Tree name="E-Commerce">
-          <div>Shopify</div>
-          <div>Wix</div>
-          <div>Custom Solutions</div>
-        </Tree>
-        <Tree name="Portfolios">
-          <div>Comedians</div>
-          <div>Photography</div>
-          <div>Legal</div>
-        </Tree>
-        <Tree name="Social Media">
-          <div>Raffles</div>
-          <div>Analytics, custom integrations</div>
-        </Tree>
-        <Tree name="Data Analysis">
-          <div>Data Cleaning</div>
-          <Tree name="Statistical Analysis">
-            <div>Single / Multiple Regression</div>
-            <div>Time Series Analysis</div>
-            <div>Logit Models</div>
-            <div>Probit Models</div>
-          </Tree>
-        </Tree>
+        <AnimatedCard front="Data Cleaning and ETL">
+          <p>I've got few years experience as a data analyst.</p>
+          <p>I'm not only a spreadsheet wiz, I also use Python (pandas) and R for the most sophisticated data needs.</p>
+        </AnimatedCard>
+
+        <AnimatedCard front="Tutor">
+          <p>Want some quick hands on tutorials on everything web dev? Data analysis?</p>
+          <p>I can teach you what I know in a one on one session!</p>
+        </AnimatedCard>
+
+        <AnimatedCard front="Network">
+          <p>Wanna connect professionally? I'm always down! 😎</p>
+        </AnimatedCard>
+
       </div>
+
     </div>
   );
 }
-
-
-Tree.propTypes = {
-  name: PropTypes.string,
-  defaultOpen: PropTypes.bool
-}
-
 
 
 export default Services;
+
