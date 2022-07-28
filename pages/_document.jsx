d0fd165c140bebeef51dc8ef4463df130e9bdbda
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-27 21:54:33 -0700

    refactored to hydrate page correctly

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-class _MyDocument extends Document {
+
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html>
@@ -22,33 +47,7 @@ class _MyDocument extends Document {
       </Html>
     );
   }
-}
 
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      })
-      const initialProps = await _MyDocument.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal();
-    }
-
-  }
 }
 
 
