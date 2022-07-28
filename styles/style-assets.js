9146bead498ae09c5c706f863191f45c3b78f976
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-27 18:20:19 -0700

    replace modules with exportable JSON

---
+++ styles/style-assets.js
@@ -0,0 +1,96 @@
+export const breakpoints = {
+
+  smallestScreenMobile: "350px",
+  smallScreenMobile: "400px",
+  bigScreenMobile: "550px",
+  tablet: "1150px",
+
+  computer: "1100px",
+  smartphoneLarge: "750px",
+  smartphoneRegular: "1100px",
+  smartphoneSmall: "1100px",
+  large: "1100px",
+
+}
+
+export const colors = {
+
+  white: "#e9e9e9",
+
+  grayLightest: "#0000",
+  grayLighter: "#0000",
+  grayLight: "#0000",
+  gray: "#0000",
+  grayDark: "#0000",
+  grayDarker: "#0000",
+  grayDarkest: "#0000",
+
+
+  blueLightest: "#0000",
+  blueLighter: "#0000",
+  blueLight: "#0000",
+  blue: "#0000",
+  blueDark: "#0000",
+  blueDarker: "#0000",
+  blueDarkest: "#0000",
+
+  redLightest: "#0000",
+  redLighter: "#0000",
+  redLight: "#0000",
+  red: "#0000",
+  redDark: "#0000",
+  redDarker: "#0000",
+  redDarkest: "#0000",
+
+  greenLightest: "#0000",
+  greenLighter: "#0000",
+  greenLight: "#0000",
+  green: "#0000",
+  greenDark: "#0000",
+  greenDarker: "#0000",
+  greenDarkest: "#0000",
+
+  purple1: "#070372",
+  purple2: "#161374",
+  purple3: "#252275",
+  purple4: "#343277",
+  purple5: "#434179",
+  purple6: "#52517a",
+  purple7: "#61607c",
+  purple8: "#70707e",
+
+  greenTriad1: "#7b751b",
+  grayTriad1: "#1b7b75",
+  purpleTriad1: "#751b7b",
+
+}
+
+
+export const fontSize = {
+  smallestScreenMobileFontsizeH1: "20px",
+  smallestScreenMobileWontsizeH2: "18px",
+  smallestScreenMobileWontsizeH3: "16px",
+  smallestScreenMobileWontsizeP: "14px",
+
+  smallScreenMobileWontsizeH1: "20px",
+  smallScreenMobileWontsizeH2: "18px",
+  smallScreenMobileWontsizeH3: "16px",
+  smallScreenMobileWontsizeP: "14px",
+
+  bigScreenMobileWontsizeH1: "24px",
+  bigScreenMobileWontsizeH2: "16px",
+  bigScreenMobileWontsizeH3: "14px",
+  bigScreenMobileWontsizeP: "12px",
+
+  tabletFontsizeH1: "24px",
+  tabletFontsizeH2: "20px",
+  tabletFontsizeH3: "18px",
+  tabletFontsizeP: "16px",
+
+  defaultFontsizeH1: "24px",
+  defaultFontsizeH2: "20px",
+  defaultFontsizeH3: "18px",
+  defaultFontsizeP: "16px",
+
+}
+
