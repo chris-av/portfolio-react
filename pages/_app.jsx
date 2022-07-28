398a06ae7bb9f30531d74bb835d2552b9fcdff4f
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-27 17:41:18 -0700

    remove unnecessary comments

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
 import Head from 'next/head'
 import GlobalStyle from '@/styles/GlobalStyle';
 
 
-// // pages
-// import AboutMe from './pages/AboutMe/AboutMe';
-// import Portfolio from './pages/Portfolio/Portfolio';
-// import Contact from './pages/Contact/Contact';
-// import Resume from './pages/Resume/Resume';
-
-// here are the components
+// components
 import Burger from '@/components/Burger.jsx';
 import Navigation from '@/components/Navigation.jsx';
 import NavigationMobile from '@/components/NavigationMobile';
 import { AppWrapper } from '@/state/state';
 
-// import styles
-// import './main.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
