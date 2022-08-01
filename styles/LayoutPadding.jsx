7f2add553120ff1f2eb1a9290bedb1a756d7fafa
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-31 22:57:01 -0700

    custom layout for padding

---
+++ styles/LayoutPadding.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function Layout({ children }) {
+  return (
+    <Container>
+      { children }
+    </Container>
+  );
+}
+
+
+const Container = styled.div`
+  padding: 25px;
+`;
+
+
+
+
+
