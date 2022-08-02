c230d2a1738c936d53b0c5e585e5c52f817d9b02
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-01 20:01:44 -0700

    new layout component

---
+++ styles/LayoutTwoPanel.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+export default function LayoutTwoPanel({ children, reverse = false }) {
+  return (
+    <Container reverse={reverse}>
+      { children }
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 3rem 0;
+  padding: 4rem;
+  min-height: 450px;
+  flex-direction: ${({ reverse}) => reverse ? "row-reverse" : "row" };
+
+  .left {
+    width: ${({reverse}) => reverse ? '60' : '50'}%;
+  }
+
+  .right {
+    width: ${({reverse}) => reverse ? '40' : '50'}%;
+  }
+
+
+  .left {
+    h1 {
+      text-align: left;
+      font-size: 4rem;
+      margin-bottom: 50px;
+    }
+
+    p {
+      font-weight: 100;
+      font-size: 1rem;
+    }
+
+    text-align: left;
+  }
+
+  .right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.2rem;
+  }
+
+  @media(max-width: 900px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+
+    .left, .right {
+      width: 100%;
+    }
+
+    .left h1 {
+      font-size: 2rem;
+    }
+
+  }
+
+`;
+
