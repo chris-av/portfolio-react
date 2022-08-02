acbe57efee049199f6f9648b1aa288c1102db876
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-01 20:15:41 -0700

    remove unused styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -68,9 +68,6 @@ const AboutMe = () => {
 const Container = styled.div`
 
   width: 100%;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
 
   .introduction {
     max-width: 750px;
