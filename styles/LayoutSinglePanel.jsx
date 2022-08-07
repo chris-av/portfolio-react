13e574c8a7162923702723353997e31aa8db2759
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-06 18:56:44 -0700

    add new layout

---
+++ styles/LayoutSinglePanel.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function LayoutSinglePanel({ children }) {
+  return (
+    <Container>
+      { children }
+    </Container>
+  );
+}
+
+
+const Container = styled.div`
+  padding: 4rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 100;
+  }
+
+  .header {}
+
+  .content {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @media (max-width: 900px) {
+    padding: 2rem;
+    h1 {
+      font-size: 2rem;
+      text-align: center;
+    }
+  }
+
+`;
+
