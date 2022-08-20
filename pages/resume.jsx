837f963cc1f573b3c9ca52d89a2f8c0a85369ed0
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-19 23:40:45 -0700

    working timeline with animation on elIsVisible

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,47 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
 
 import LayoutPadding from '@/styles/LayoutPadding';
+import useIsVisible from '@/hooks/useIsVisible';
 
 import { resume } from '@/data/resume.data';
 
 const Resume = () => {
-  
-  return (
-    <LayoutPadding>
-      <Container>
-        <div className="timeline">
 
-          { resume.jobs.map(j => (
-            <TimeLineSection 
-              id={j.id} 
-              organization={j.organization}
-              jobtitle={j.jobtitle} 
-              range={j.range} 
-              description={j.description} 
-            />
-          )) }
-      
-        </div>
-      </Container>
-    </LayoutPadding>
+  const testRef1 = useRef();
+  const testRef2 = useRef();
+  const testRef3 = useRef();
+
+
+  const testRefIsVisible1 = useIsVisible(testRef1);
+  const testRefIsVisible2 = useIsVisible(testRef2);
+  const testRefIsVisible3 = useIsVisible(testRef3);
+
+  const refs = [ testRef1, testRef2, testRef3 ];
+  const visibles = [ testRefIsVisible1, testRefIsVisible2, testRefIsVisible3 ];
+
+  console.log({ testRefIsVisible1, testRefIsVisible2, testRefIsVisible3 });
+
+  return (
+    <Container>
+
+      <h1 className="work-history">Work History</h1>
+
+      <div className="timeline">
+
+        { resume.jobs.map((j, i) => (
+          <TimeLineSection 
+            id={j.id} 
+            organization={j.organization}
+            jobtitle={j.jobtitle} 
+            range={j.range} 
+            description={j.description} 
+            myref={refs[i]}
+            isVisible={visibles[i]}
+          />
+        )) }
+    
+      </div>
+    </Container>
   )
 }
 
 
-const TimeLineSection = ({ id, organization, jobtitle, range, description }) => {
+const TimeLineSection = ({ id, organization, jobtitle, range, description, myref, isVisible }) => {
+
+  const animate = {
+    opacity: isVisible ? 1 : 0,
+    y: isVisible ? 0 : 24,
+    duraitno: 2000,
+  }
+
   return (
     <div className="timeline-section" key={id}>
       <div className="time-range">{ range }</div>
       <div className="line"></div>
-      <div className="job">
-        <h1>{jobtitle}</h1>
+      <animated.div className="job" style={useSpring({ ...animate, delay: 300 })}>
+        <h1 ref={myref}>{jobtitle}</h1>
         <h2>{organization}</h2>
         <ul>
           { description.map((des, i) => <li key={i}>{ des }</li>) }
         </ul>
-      </div>
+      </animated.div>
     </div>
   );
 }
@@ -50,6 +75,13 @@ const TimeLineSection = ({ id, organization, jobtitle, range, description }) => 
 
 const Container = styled.div`
 
+  .work-history {
+    width: 100%;
+    text-align: center;
+    font-size: 2.5rem;
+    margin: 4rem 0;
+  }
+
   .timeline {
     width: 100%;
   }
@@ -57,19 +89,28 @@ const Container = styled.div`
   .timeline-section {
     display: flex;
     flex-direction: row;
+    min-height: 100vh;
   }
 
   .time-range {
     font-size: 1rem;
+    font-weight: bold;
+    font-style: italic;
     text-align: right;
-    min-width: 250px;
+    max-width: 250px;
+    min-width: 20%;
     padding: 5px;
   }
 
   .job {
-    h1 { font-size: 1.6rem; }
-    h2 { font-size: 1.4rem; }
-    ul li { font-size: 1rem; }
+    h1, h2 { margin: 0; padding: 0; }
+
+    h1 { font-size: 1.6rem; margin-bottom: 10px; }
+    h2 { font-size: 1.2rem; }
+    ul li {
+      font-size: 1rem;
+      margin: 10px 0;
+    }
     margin-bottom: 100px;
   }
 
@@ -77,6 +118,18 @@ const Container = styled.div`
     margin: 0 15px;
     width: 1px;
     background-color: black;
+  }
+
+
+  @media (max-width: 900px) {
+    .time-range {
+      font-size: 0.8rem;
+    }
+    .job {
+      h1 { font-size: 1rem; }
+      h2 { font-size: 1rem; }
+      ul li { font-size: 0.8rem; }
+    }
   }
 
 `;
