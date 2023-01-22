84ab83f725ddc6747711a93b21962e39f4ef9297
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-21 22:33:58 -0800

    replace styled-components with tailwind

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
 
 
 
-const Navigation = () => {
+export default function Navigation() {
     return (
-      <Container className="container">
-
-        <nav className="navbar">
+      <div className="">
+        <nav className="flex justify-between min-h-[80px] bg-purple text-white">
           
-          <Link href="/" className="navbar-brand">
+          <Link href="/" className="flex justify-center items-center select-none text-2xl ml-6">
             Christian Valenzuela
           </Link>
           
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link href="/" className="nav-link">
+          <ul className="flex justify-between items-center">
+            <li className="flex justify-center items-center h-full min-w-[120px]">
+              <Link href="/" className="h-full flex items-center">
                 About Me
               </Link>
             </li>
-            <li className="nav-item">
-              <Link href="/portfolio" className="nav-link">
+            <li className="flex justify-center items-center h-full min-w-[120px]">
+              <Link href="/portfolio" className="h-full flex items-center">
                 My Portfolio
               </Link>
             </li>
-            <li className="nav-item">
-              <Link href="/resume" className="nav-link">
+            <li className="flex justify-center items-center h-full min-w-[120px]">
+              <Link href="/resume" className="h-full flex items-center">
                 Resume
               </Link>
             </li>
-            <li className="nav-item">
-              <Link href="/contact" className="nav-link">
+            <li className="flex justify-center items-center h-full min-w-[120px]">
+              <Link href="/contact" className="h-full flex items-center">
                 Contact
               </Link>
             </li>
@@ -39,70 +37,7 @@ const Navigation = () => {
 
         </nav>
 
-      </Container>
+      </div>
     );
 }
 
-
-const Container = styled.div`
-  
-  width: 100%;
-
-  .navbar {
-    width: 100%;
-    background-color: #434179;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    min-height: 80px;
-  }
-
-  .navbar-brand { 
-    color: #e9e9e9;
-    user-select: none;
-    text-decoration: none;
-    font-size: 20px;
-    font-weight: 800;
-    margin: auto 0;
-    margin-left: 50px;
-  }
-
-  .nav-item {
-    height: 100%;
-    display: flex; justify-content: center; align-items: center;
-    min-width: 120px;
-    &:hover { background-color: #434179; cursor: pointer; }
-  }
-
-  .nav-link {
-    text-decoration: none; 
-    color: inherit; color: #e9e9e9;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .navbar-nav {
-    display: flex;
-    flex-direction: row;
-    justify-items: center;
-    align-items: center;
-    list-style: none;
-  }
-
-
-    
-  @media only screen and (max-width: 1100px) {
-    
-    display: none;
-
-  }
-
-
-`;
-
-
-
-export default Navigation;
