3edf5abff59684f9ed5aa211c38cf865fcc6704b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-21 23:15:21 -0800

    use tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head'
+import '@/styles/globals.css';
 import GlobalStyle from '@/styles/GlobalStyle';
 import styled from 'styled-components';
 
