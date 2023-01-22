7c02f398a336c425ae6cac1d9236559b78f14b47
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-22 01:30:17 -0800

    replace styled-components with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,16 @@
-import React, { useRef } from 'react';
-import styled from 'styled-components';
-import { useAppContext } from '@/state/state';
-import { github, instagram, linkedin } from '@/data/socials';
-
-
 import Introduction from '@/components/Introduction';
 import Cryptos from '@/components/Cryptos';
 import Services from '@/components/Services';
+import { github, instagram, linkedin } from '@/data/socials';
 
-import Message from '@/components/Message';
 
-
-const AboutMe = () => {
-
-  
+export default function AboutMe() {
   return (
-    <Container>
+    <div>
           
-      <Introduction />
+      <div className="">
+        <Introduction />
+      </div>
 
       <Services />
 
@@ -25,39 +18,15 @@ const AboutMe = () => {
 
       <Cryptos />
 
-      <div className="footer-container">
+      <div className="w-full flex justify-center py-14">
         <a target="_blank" rel="noopener noreferrer" href={linkedin}><i className={`linkedin link big black icon`}></i></a>
         <a target="_blank" rel="noopener noreferrer" href={github}><i className={`github link big black icon`}></i></a>
         <a target="_blank" rel="noopener noreferrer" href={instagram}><i className={`instagram link big black icon`}></i></a>
       </div>
 
 
-    </Container>
+    </div>
   );
 }
 
-
-const Container = styled.div`
-
-  width: 100%;
-
-  .introduction {
-    max-width: 750px;
-  }
-
-  h1 { text-align: center; }
-
-  .footer-container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 40px 0;
-  }
-
-
-`;
-
-
-
-export default AboutMe;
 
