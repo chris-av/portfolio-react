f5a50fc2b630353641c914246ad05f67cca9f60c
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-21 22:33:37 -0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          DEFAULT: "#434179",
+        },
+        white: {
+          DEFAULT: "#e9e9e9",
+        }
+      }
+    },
   },
   plugins: [],
 }
