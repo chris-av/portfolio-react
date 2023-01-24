099d42ced78e5142bb759e7f1a36a84210d2f206
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-24 00:18:35 -0800

    create exportable wallet data

---
+++ data/wallets.data.ts
@@ -0,0 +1,5 @@
+export const wallets = {
+  bitcoin: '36VbQZMPk69SLVauprfLZFu4yk1cFmUCwm',
+  ethereum: '0x8f6f684ce9df9483753331D78b29367B4AE7A1Ef',
+  doge: 'DMavjUyrMdkqYHojfBRR4Uu6742tVswqGW'
+}
