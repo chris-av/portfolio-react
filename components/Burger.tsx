748ee2aa616b570fb31b31583761f735369cfe1b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-28 22:12:23 -0800

    move burger to inside navigation

--- components/Burger.tsx
+++ components/Burger.tsx
@@ -4,13 +4,13 @@ import { useAppContext } from '@/state/state'
 export default function Burger() {
   const isExpanded = true;
   const { open, toggleOpen } = useAppContext();
-  
+
   return (
-    <div 
-      aria-label="Toggle menu" 
-      aria-expanded={isExpanded} 
+    <div
+      aria-label="toggle menu"
+      aria-expanded={isExpanded}
       onClick={() => toggleOpen(!open)}
-      className="lg:hidden bg-purple flex flex-col justify-center w-full h-[80px] outline-transparent pl-8 [&>*]:bg-white [&>*]:h-[4px] [&>*]:w-[35px] [&>*]:my-1 [&>*]:rounded-lg [&>*]:transition-transform [&>*]:duration-300 [&>*]:origin-[3px]"
+      className="h-full outline-transparent w-[80px] cursor-pointer flex flex-col justify-center pl-8 [&>*]:bg-white [&>*]:h-[4px] [&>*]:w-[35px] [&>*]:my-1 [&>*]:rounded-lg [&>*]:transition-transform [&>*]:duration-300 [&>*]:origin-[3px]"
     >
       <span style={{ transform: open ? 'rotate(45deg)' : 'rotate(0)' }} />
       <span style={{ transform: open ? 'translateX(20px)' : 'translateX(0)', opacity: open ? 0 : 1 }} />
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
-
+import Burger from '@/components/Burger';
 
 
 export default function Navigation() {
     return (
-      <div className="hidden lg:block">
-        <nav className="flex justify-between min-h-[80px] bg-purple text-white">
+      <div className="fixed w-full lg:static h-[80px] bg-purple text-white z-30">
+
+        <div className="h-full lg:hidden">
+          <Burger />
+        </div>
+
+        <nav className="hidden h-full lg:flex justify-between">
           
           <Link href="/" className="flex justify-center items-center select-none text-2xl ml-6 hover:text-white">
             Christian Valenzuela
