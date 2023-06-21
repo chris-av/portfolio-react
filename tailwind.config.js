2b5a2cb3c6b8b53f28728549ae9fc2bafc938728
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-06-20 18:45:59 -0700

    add color to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,10 @@ module.exports = {
         },
         white: {
           DEFAULT: "#e9e9e9",
-        }
+        },
+        green: {
+          DEFAULT: "#13C253",
+        },
       }
     },
   },
