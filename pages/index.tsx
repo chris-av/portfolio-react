ee1196e04bc1cab8cdc3eae6ea1259cb5c3e9690
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-06-20 18:45:54 -0700

    reorder classes due to plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,17 @@ import { github, instagram, linkedin } from '@/data/socials';
 export default function AboutMe() {
   return (
     <div>
-          
       <div className="">
         <Introduction />
       </div>
 
       <Services />
 
-      <h1 className="text-4xl text-center mt-24 mb-12">Wanna donate ? </h1>
+      <h1 className="mb-12 mt-24 text-center text-4xl">Wanna donate ? </h1>
 
       <Cryptos />
 
-      <div className="w-full flex justify-center py-14">
+      <div className="flex w-full justify-center py-14">
         <a target="_blank" rel="noopener noreferrer" href={linkedin}><i className={`linkedin link big black icon`}></i></a>
         <a target="_blank" rel="noopener noreferrer" href={github}><i className={`github link big black icon`}></i></a>
         <a target="_blank" rel="noopener noreferrer" href={instagram}><i className={`instagram link big black icon`}></i></a>
