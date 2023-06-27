65c98f8bb2503fcb91642205152d573510c07bb7
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-06-26 23:02:32 -0700

    use client wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-import Introduction from '@/components/Introduction';
-import Cryptos from '@/components/Cryptos';
-import Services from '@/components/Services';
-import { github, instagram, linkedin } from '@/data/socials';
-
+import ClientWrapper from './ClientWrapper';
 
 export default function Page() {
-
   return (
     <div>
-      <div className="">
-        <Introduction />
-      </div>
-
-      <Services />
-
-      <h1 className="mb-12 mt-24 text-center text-4xl">Wanna donate ? </h1>
-
-      <Cryptos />
-
-      <div className="flex w-full justify-center py-14">
-        <a target="_blank" rel="noopener noreferrer" href={linkedin}><i className={`linkedin link big black icon`}></i></a>
-        <a target="_blank" rel="noopener noreferrer" href={github}><i className={`github link big black icon`}></i></a>
-        <a target="_blank" rel="noopener noreferrer" href={instagram}><i className={`instagram link big black icon`}></i></a>
-      </div>
-
+      <ClientWrapper />
     </div>
   );
 }
