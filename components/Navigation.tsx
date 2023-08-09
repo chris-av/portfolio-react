569113cf02f792536211c8075d7080cbb0f07548
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-08-08 22:56:52 -0700

    navbar should have provider to propogate state

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,47 +1,54 @@
+"use client";
 import Link from 'next/link';
 import Burger from '@/components/Burger';
+import NavigationMobile from './NavigationMobile';
+import NavbarProvider from '@/state/navbar';
 
 
 export default function Navigation() {
   return (
-    <div className="fixed z-30 h-[80px] w-full bg-purple text-white lg:static">
+    <NavbarProvider>
+      <div className="fixed z-30 h-[80px] w-full bg-purple text-white lg:static">
+        <NavigationMobile />
 
-      <div className="h-full lg:hidden">
-        <Burger />
+        <div className="absolute z-50 h-full lg:hidden">
+          <Burger />
+        </div>
+
+        <nav className="hidden h-full justify-between lg:flex">
+
+          <Link href="/" className="ml-6 flex select-none items-center justify-center text-2xl hover:text-[#DFDFDF]">
+            Christian Valenzuela
+          </Link>
+
+          <ul className="flex items-center justify-between">
+            <li className="flex h-full min-w-[120px] items-center justify-center">
+              <Link href="/" className="flex h-full items-center hover:text-[#DFDFDF]">
+                About Me
+              </Link>
+            </li>
+            <li className="flex h-full min-w-[120px] items-center justify-center">
+              <Link href="/portfolio" className="flex h-full items-center hover:text-[#DFDFDF]">
+                My Portfolio
+              </Link>
+            </li>
+            <li className="flex h-full min-w-[120px] items-center justify-center">
+              <Link href="/resume" className="flex h-full items-center hover:text-[#DFDFDF]">
+                Resume
+              </Link>
+            </li>
+            <li className="flex h-full min-w-[120px] items-center justify-center">
+              <Link href="/contact" className="flex h-full items-center hover:text-[#DFDFDF]">
+                Contact
+              </Link>
+            </li>
+          </ul>
+
+        </nav>
+
       </div>
 
-      <nav className="hidden h-full justify-between lg:flex">
-
-        <Link href="/" className="ml-6 flex select-none items-center justify-center text-2xl hover:text-[#DFDFDF]">
-          Christian Valenzuela
-        </Link>
-
-        <ul className="flex items-center justify-between">
-          <li className="flex h-full min-w-[120px] items-center justify-center">
-            <Link href="/" className="flex h-full items-center hover:text-[#DFDFDF]">
-              About Me
-            </Link>
-          </li>
-          <li className="flex h-full min-w-[120px] items-center justify-center">
-            <Link href="/portfolio" className="flex h-full items-center hover:text-[#DFDFDF]">
-              My Portfolio
-            </Link>
-          </li>
-          <li className="flex h-full min-w-[120px] items-center justify-center">
-            <Link href="/resume" className="flex h-full items-center hover:text-[#DFDFDF]">
-              Resume
-            </Link>
-          </li>
-          <li className="flex h-full min-w-[120px] items-center justify-center">
-            <Link href="/contact" className="flex h-full items-center hover:text-[#DFDFDF]">
-              Contact
-            </Link>
-          </li>
-        </ul>
-
-      </nav>
-
-    </div>
+    </NavbarProvider>
   );
 }
 
