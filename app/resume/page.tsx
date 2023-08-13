a46cc714f5284d1db16a6e1c6090b9e83fca89fa
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-08-13 13:06:38 -0700

    lower margin

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,7 +3,7 @@ import TimeLineSection from './TimeLineSection';
 export default function Page() {
   return (
     <div>
-      <h1 className="my-24 text-center text-4xl">Work History</h1>
+      <h1 className="my-20 text-center text-4xl">Work History</h1>
       <div className="">
         {resume.jobs.map(({ id, organization, jobtitle, range, description }) => (
           <TimeLineSection
