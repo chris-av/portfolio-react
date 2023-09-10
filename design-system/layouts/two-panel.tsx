867fa8c21623a3102c01662412f15b659d61d562
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-09-10 13:41:12 -0700

    migrate layout component

---
+++ design-system/layouts/two-panel.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import classnames from 'classnames';
+
+interface Props {
+  children: ReactNode;
+}
+
+export function LayoutTwoPanel({
+  children,
+}: Props) {
+  return (
+    <div
+      className={classnames(
+        "my-12 flex min-h-[450px] w-full flex-col items-center justify-between p-12 lg:flex-row",
+        "[&>*]:w-full lg:[&>*]:w-1/2"
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+
