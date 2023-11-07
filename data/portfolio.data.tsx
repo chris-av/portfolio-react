b377607227595d8cc17b629a7030c37c1bd1d7b1
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-11-07 00:04:27 -0800

    add new gigs and tags

--- data/portfolio.data.tsx
+++ data/portfolio.data.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 export const portfolio = [
   {
+    id: "b07e8c1e14fbdbc5e3be",
+    organization: "Eminent Translations",
+    active: true,
+    jobtitle: "Independent Contractor - Developer",
+    description: () => (
+      <div>
+        <p>Modernized backend, using Sanity as a headless</p>
+        <p>Worked closely with the college admninistrators to understand the business logic and reimplement using React for frontend and Node/Express + Postgres.</p>
+        <p>Coordinated with students and teachers to incorporate feedback and implement new features.</p>
+      </div>
+    ),
+    delay: 0,
+    tags: ["NodeJS", "Express", "React", "Azure", "C#", "Webhooks", "REST"]
+  },
+  {
     id: "49b9a8b5393675c47c69",
     organization: "South Coast College",
     active: true,
@@ -14,6 +29,7 @@ export const portfolio = [
       </div>
     ),
     delay: 0,
+    tags: ["NodeJS", "Express", "React", "Azure", "C#", "Webhooks", "REST"]
   },
   {
     id: "e34b074a4f4e0035be2b",
@@ -28,7 +44,21 @@ export const portfolio = [
         <p>Started as a front end project but ultimately became a full stack, React-Mongo project to fulfill client requirements for an interactive experience and mailing solution.</p>
       </div>
     ),
-    delay: 1000
+    delay: 1000,
+    tags: ["React", "NextJS", "MongoDB", "Sanity.io", "Webhooks", "SSR"]
+  },
+  {
+    id: "aa40d71a2934be5e0444",
+    organization: "Lands End",
+    active: true,
+    jobtitle: "Web Developer",
+    description: () => (
+      <div>
+        <p>Landing page for real estate firm specializing in the acquisition of industrial estate and adding value for prospective clients.</p>
+      </div>
+    ),
+    delay: 1000,
+    tags: ["Webflow", "SEO", "CMS"]
   },
   {
     id: "32e533eba2a7dce65caf",
@@ -43,7 +73,8 @@ export const portfolio = [
         <p>Scaffolded an integrated ReactJS and Firebase project structure for the app itself and offered technical support and advice for the team that took the project on full time.</p>
       </div>
     ),
-    delay: 1200
+    delay: 1200,
+    tags: ["React", "Google Firebase", "Express", "NodeJS"]
   },
   {
     id: "2678aa159f464ce6de5b",
