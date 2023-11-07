c963bc167ad7705a44292484f7052ef25eacb3b0
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-11-07 00:01:28 -0800

    accommodate alignment on all screen sizes

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -5,7 +5,7 @@ import { useTransition, animated } from '@react-spring/web';
 
 export default function Introduction({ messageRef }: { messageRef: any }) {
 
-  const [ items, setItems ] = useState<"Developer" | "Analyst" | "Engineer">('Analyst');
+  const [items, setItems] = useState<"Developer" | "Analyst" | "Engineer">('Analyst');
   const transitions = useTransition(items, {
     from: { opacity: 0, y: 50 },
     enter: { opacity: 1, y: 0 },
@@ -19,7 +19,7 @@ export default function Introduction({ messageRef }: { messageRef: any }) {
   }
 
   const colors = {
-    "Developer": "#C85D91",  
+    "Developer": "#C85D91",
     "Analyst": "#5D91C8",
     "Engineer": "#91C85D",
   } as const;
@@ -37,23 +37,30 @@ export default function Introduction({ messageRef }: { messageRef: any }) {
   }, [items]);
 
   return (
-    <div className="h-screen p-12">
-      <div className="flex h-5/6 items-center">
-        <h1 className="-left-2 -top-20 text-2xl sm:text-4xl md:left-20 md:text-6xl xl:text-8xl">Professional &nbsp;
-          { transitions((style, item) => (
-            <animated.span style={{ ...style, color: colors[item] }} className="absolute">
-              { item }
+    <div className="relative h-screen">
+      <div className="relative flex h-5/6 flex-col items-center justify-center text-center lg:ml-[15%] lg:text-left">
+        <h1 className="-top-20 w-full text-6xl xl:text-8xl">Professional&nbsp;
+          {transitions((style, item) => (
+            <animated.span style={{ ...style, color: colors[item] }} className="absolute hidden lg:inline-block">
+              {item}
             </animated.span>
-          )) }
+          ))}
         </h1>
 
-        <div className="absolute bottom-10 right-1/2 -mr-10">
-          <i 
-            className="angle down huge icon cursor-pointer" 
-            onClick={() => scrollToRef()}
-          />
+        <div className="my-12 text-6xl font-bold lg:hidden xl:text-8xl">
+          {transitions((style, item) => (
+            <animated.div style={{ ...style, color: colors[item] }} className="absolute left-0 w-full">
+              {item}
+            </animated.div>
+          ))}
         </div>
-        
+      </div>
+
+      <div className="bottom-50 absolute right-1/2 -mr-10">
+        <i
+          className="angle down huge icon cursor-pointer"
+          onClick={() => scrollToRef()}
+        />
       </div>
     </div>
   );
