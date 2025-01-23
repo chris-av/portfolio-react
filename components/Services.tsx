703e3ca13a853436d1e93591b07dfcb92e4f5681
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-22 20:11:56 -0800

    simplify component for now

--- components/Services.tsx
+++ components/Services.tsx
@@ -40,7 +40,19 @@ export default function Services({ messageRef }: { messageRef: MutableRefObject<
           <p>I have substantial experience using advanced Javascript libraries and frameworks like React, Nextjs and or others to ensure that your website's codebase is based on the bleeding edge of web technologies!</p>
         </div>
         <div className="flex w-1/2 items-center justify-center" ref={webappRef}>
-          <WebAppsAnimation isVisible={state1} />
+          <CreateAnimationSection
+            isVisible={state1}
+            start_delay={300}
+            next_item_delay={300}
+            assets={[
+              { id: "6498c3e745", type: "semantic", icon: "react", color: "blue" },
+              { id: "4d73b78ab7", type: "semantic", icon: "node", color: "green" },
+              { id: "ab9c5b5561", type: "semantic", icon: "js", color: "yellow" },
+              { id: "72def0fac4", type: "semantic", icon: "html5", color: "red" },
+              { id: "5afc7ad30c", type: "semantic", icon: "css3", color: "blue" },
+              { id: "041a21c0ce", type: "semantic", icon: "sass", color: "pink" },
+            ]}
+          />
         </div>
       </LayoutTwoPanel>
 
@@ -53,155 +65,86 @@ export default function Services({ messageRef }: { messageRef: MutableRefObject<
           <p>Whether it is ETL, data exploration visualization, or analysis, I have the expertise to create dashboards, reports or other assets to help you understand your own data.</p>
         </div>
         <div className="flex w-1/2 items-center justify-center" ref={dataRef}>
-          <DataAnimation isVisible={state2} />
+          <CreateAnimationSection
+            isVisible={state2}
+            start_delay={600}
+            next_item_delay={300}
+            assets={[
+              { id: "78ab7", type: "semantic", icon: "python", color: "blue" },
+              { id: "78ab7", type: "semantic", icon: "database", color: "black" },
+              { id: "3e745", type: "custom", src: "/Rlogo.svg", styles: { width: "80px", height: "64px" }, },
+              { id: "3e745", type: "custom", src: "/noun-data-analysis-2790390.svg", styles: { width: "80px", height: "64px" }, },
+              { id: "ad30c", type: "semantic", icon: "chart pie", color: "black" },
+              { id: "1c0ce", type: "semantic", icon: "cloud", color: "black" },
+            ]}
+          />
         </div>
       </LayoutTwoPanel>
 
-      <div className="p-[4rem]">
-        <div className="header w-full">
-          <h1 className="mb-12 text-center text-xl md:text-3xl">General Technologist</h1>
-          <p className="">I love technology! Whatever solution you need&#8212;whether that by a small script, an ad hoc analysis, or a full web app&#8212;I am always up to the challenge to find a solution.</p>
-          <p className="">I have experience using different platforms to deploy my projects and ensure a sensible, efficient develop/deployment pipeline.</p>
-        </div>
-        <div className="mx-auto flex w-full max-w-[800px] justify-center" ref={techRef}>
-          <TechnologistAnimation isVisible={state3} />
-        </div>
-      </div>
-
     </div>
   );
 }
 
+type SemanticIcon = {
+  id: string;
+  type: "semantic";
+  icon: string;
+  color: string;
+}
 
-const WebAppsAnimation = ({ isVisible }: { isVisible: boolean; }) => {
+type CustomIcon = {
+  id: string;
+  type: "custom";
+  src: string;
+  styles?: Record<string, string>;
+}
 
+type IconAsset = SemanticIcon | CustomIcon;
+
+interface CreateAnimationSectionProps {
+  isVisible: boolean;
+  start_delay: number;
+  next_item_delay: number;
+  assets: IconAsset[]
+}
+
+const CreateAnimationSection = ({
+  isVisible,
+  start_delay,
+  next_item_delay,
+  assets,
+}: CreateAnimationSectionProps) => {
   const animate = {
     opacity: isVisible ? 1 : 0,
     y: isVisible ? 0 : 24,
     duration: 2000
   };
-
-  const icons = [
-    { id: "6498c3e745", icon: "react", color: "blue" },
-    { id: "4d73b78ab7", icon: "node", color: "green" },
-    { id: "ab9c5b5561", icon: "js", color: "yellow" },
-    { id: "72def0fac4", icon: "html5", color: "red" },
-    { id: "5afc7ad30c", icon: "css3", color: "blue" },
-    { id: "041a21c0ce", icon: "sass", color: "pink" },
-  ];
 
   return (
     <div className="grid grid-cols-3 gap-12">
-      {icons.map(({ id, icon, color }, i) => (
+      {assets.map((asset, i) => (
         <animated.div
-          key={id}
-          style={useSpring({...animate, delay: 300 + (300*i)})}
+          key={asset.id}
+          style={useSpring({ ...animate, delay: start_delay + (next_item_delay * i) })}
         >
-          <i className={classnames([
-            icon, color,
-            "huge", // TODO: detect screen size dynamically
-            "icon",
-          ])}/>
+          {asset.type === "semantic" ? (
+            <i
+              className={classnames([
+                asset.icon, asset.color,
+                "huge",
+                "icon",
+              ])}
+            />
+          ) : (
+            <img
+              className=""
+              src={asset.src}
+              style={asset.styles}
+            />
+          )}
         </animated.div>
       ))}
     </div>
-  );
+  )
 
 }
-
-
-const DataAnimation = ({ isVisible }: { isVisible: boolean; }) => {
-
-  const animate = {
-    opacity: isVisible ? 1 : 0,
-    y: isVisible ? 0 : 24,
-    duration: 2000
-  };
-
-  return (
-    <div className="grid grid-cols-3 gap-12 [&>*]:flex [&>*]:items-center [&>*]:justify-center">
-      <animated.img style={{ ...useSpring({ ...animate, delay: 300 }), width: "80px" }} src="/python-logo-notext.svg" className="h-[64px]" />
-      <animated.div style={useSpring({ ...animate, delay: 1400 })}>
-        <i className="database black huge icon"></i>
-      </animated.div>
-      <animated.img style={{ ...useSpring({ ...animate, delay: 1000 }), width: "80px" }} src="/Rlogo.svg" className="h-[64px]" />
-      <animated.img style={{ ...useSpring({ ...animate, delay: 800 }), width: "80px" }} src="/noun-data-analysis-2790390.svg" className="h-[64px]" />
-      <animated.div style={useSpring({ ...animate, delay: 1400 })}>
-        <i className="chart pie black huge icon"></i>
-      </animated.div>
-      <animated.div style={useSpring({ ...animate, delay: 1400 })}>
-        <i className="cloud black huge icon"></i>
-      </animated.div>
-    </div>
-  );
-
-}
-
-const TechnologistAnimation = ({ isVisible }: { isVisible: boolean; }) => {
-
-  const animate = {
-    opacity: isVisible ? 1 : 0,
-    y: isVisible ? 0 : 24,
-    duration: 2000
-  };
-
-  return (
-    <div className="mx-auto">
-      <h2 className="my-8 mt-24 text-center text-2xl">Databases and Version Control</h2>
-
-      <div className="grid grid-cols-3 gap-20 [&>*]:flex [&>*]:items-center [&>*]:justify-center">
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="git icon huge"></animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="github icon huge"></animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="gitlab orange icon huge"></animated.i>
-        </div>
-
-        <animated.i style={useSpring({ ...animate, delay: 500 })} >
-          <img className="postgres" src="/postgres.svg" style={{ height: "80px" }} />
-        </animated.i>
-        <animated.i style={useSpring({ ...animate, delay: 500 })} >
-          <img className="mongodb" src="/MongoDB_Icon.svg" style={{ height: "80px" }} />
-        </animated.i>
-        <animated.i style={useSpring({ ...animate, delay: 500 })} >
-          <img className="firebase" src="/Firebase_Logo.svg" style={{ height: "80px" }} />
-        </animated.i>
-
-      </div>
-
-      <h2 className="my-8 mt-24 text-center text-2xl">Deployment</h2>
-      <div className="mx-auto grid w-full grid-cols-3 gap-12 [&>*]:flex [&>*]:items-center [&>*]:justify-center">
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="docker blue icon huge"></animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="cpanel orange icon huge"></animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="aws yellow icon huge"></animated.i>
-        </div>
-
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })} className="linode green icon huge"></animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })}>
-            <img className="netlify" src="/netlify-logomark.svg" style={{ height: "80px" }} />
-          </animated.i>
-        </div>
-        <div>
-          <animated.i style={useSpring({ ...animate, delay: 300 })}>
-            <img className="heroku" src="/heroku-logotype-vertical-purple.svg" style={{ height: "80px" }} />
-          </animated.i>
-        </div>
-
-      </div>
-
-    </div>
-  );
-}
-
-
