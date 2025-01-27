a11d6edfe5de915f135e3d685343c046dbbdbac6
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-27 00:22:54 -0800

    remove

--- data/wallets.data.ts
+++
@@ -1,5 +0,0 @@
-export const wallets = {
-  bitcoin: '36VbQZMPk69SLVauprfLZFu4yk1cFmUCwm',
-  ethereum: '0x8f6f684ce9df9483753331D78b29367B4AE7A1Ef',
-  doge: 'DMavjUyrMdkqYHojfBRR4Uu6742tVswqGW'
-}
