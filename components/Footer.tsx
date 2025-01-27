fcf6209d8f57a05639e30a93a23e2edb05d55f9c
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-26 23:20:13 -0800

    remove instagram

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,10 @@
-import { github, instagram, linkedin } from '@/data/socials';
+import { github, linkedin } from '@/data/socials';
 
 export default function Footer() {
   return (
     <div className="flex w-full justify-center py-14">
       <a target="_blank" rel="noopener noreferrer" href={linkedin}><i className={`linkedin link big black icon`}></i></a>
       <a target="_blank" rel="noopener noreferrer" href={github}><i className={`github link big black icon`}></i></a>
-      <a target="_blank" rel="noopener noreferrer" href={instagram}><i className={`instagram link big black icon`}></i></a>
     </div>
   );
 }
