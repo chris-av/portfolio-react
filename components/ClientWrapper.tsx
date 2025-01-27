61c9cd943d2e15c4309fa15072693943d4061288
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-27 00:24:49 -0800

    fix typo

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 interface Props {
   children: ReactNode;
 }
-export default function ClienWrapper({ children }: Props) {
+export default function ClientWrapper({ children }: Props) {
   return (
     <div>
       {children}
