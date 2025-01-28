90cd08fb0a21a0154d192839cb001aa8eda2f195
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-28 01:30:15 -0800

    update text description

--- data/resume.data.ts
+++ data/resume.data.ts
@@ -19,11 +19,12 @@ export const freelance: Job[] = [
     jobtitle: "Full Stack Engineer",
     range: "November 2022 - May 2024",
     description: [
-      "Modernized backend, using Sanity as a headless",
-      "Worked closely with the college admninistrators to understand the business logic and reimplement using React for frontend and Node/Express + Postgres.",
-      "Coordinated with students and teachers to incorporate feedback and implement new features.",
+      "Improved load times by 80% by using NextJS, moving client-side computations off of the client into the server and lowering bundle size.",
+      "Modernized backend, using Sanity as a headless CMS solution, empowering the client to define their own schemas.",
+      "Implemented a cohesive design system, reducing the amount of stylesheet code by 23%.",
+      "Created an interactive, reader-like experience for users to navigate chapters and volumes of content.",
     ],
-    tags: ["NodeJS", "Express", "React", "Azure", "C#", "Webhooks", "REST"]
+    tags: ["NextJS", "TypeScript", "server components", "Sanity.io", "Vercel", "caching",],
   },
   {
     id: "49b9a8b5393675c47c69",
@@ -31,11 +32,12 @@ export const freelance: Job[] = [
     range: "January 2022 - March 2022",
     jobtitle: "Full Stack Engineer",
     description: [
-      "Converted an out-of-commission C# application into a full stack Single Page App written in Javascript (React).",
-      "Worked closely with the college admninistrators to understand the business logic and reimplement using React for frontend and Node/Express + Postgres.",
-      "Coordinated with students and teachers to incorporate feedback and implement new features.",
+      "Authored an interactive leaderboard for real time words per minute rankings for the college’s stenography program.",
+      "Performed business analysis with college administrators, students and other stakeholders.",
+      "Implemented a ReactJS frontend and NodeJS backend architecture for simplicity, composability, and scalability.",
+      "Deployed application via Azure and designed a CI/CD pipeline to decrease deployment times and catch errors in development.",
     ],
-    tags: ["NodeJS", "Express", "React", "Azure", "C#", "Webhooks", "REST"]
+    tags: ["ReactJS", "NodeJS", "ExpressJS", "Azure Cloud Services", "PostgreSQL", "CI/CD", "GitHub", "Git", "GitHub Actions", "Wireframes", "REST API",],
   },
   {
     id: "e34b074a4f4e0035be2b",
@@ -43,20 +45,21 @@ export const freelance: Job[] = [
     range: "August 2020 - February 2021",
     jobtitle: "Frontend Engineer",
     description: [
-      "Landing page for an online proctoring solution geared towards educators.",
-      "Started as a front end project but ultimately became a full stack, React - Mongo project to fulfill client requirements for an interrange experience and mailing solution.",
+      "Created promotional website for ed-tech startup using NextJS to leverage caching and server-side rendering, improving SEO ranking and user experience.",
+      "Architected backend service to ingest prospect emails via a custom subscription service, ensuring a successful marketing push in the early state of the startup.",
+      "Implemented headless CMS solution for managing the site’s blog, increasing the efficiency of the editing process and lower time to publish by 40%.",
     ],
-    tags: ["React", "NextJS", "MongoDB", "Sanity.io", "Webhooks", "SSR"]
-  },
+    tags: ["React", "NextJS", "ExpressJS", "TailwindCSS", "REST", "SEO", "Headless CMS", "Sanity.io", ], },
   {
     id: "32e533eba2a7dce65caf",
     organization: "Lands End",
     range: "",
     jobtitle: "Web Developer",
     description: [
-      "Landing page for real estate firm specializing in the acquisition of industrial estate and adding value for prospective clients."
+      "Landing page for real estate firm specializing in the acquisition of industrial estate.",
+      "Increased Search Engine Optimization rankings, leading to increased leads and prospective clientele.",
     ],
-    tags: ["Webflow", "SEO", "CMS"]
+    tags: ["Webflow", "search engine optimization", "content management system",],
   },
   {
     id: "cf8d31eebf514038289a;",
@@ -64,10 +67,10 @@ export const freelance: Job[] = [
     range: "",
     jobtitle: "Frontend Engineer",
     description: [
-      "Landing page for a wellness app",
-      "Scaffolded an integrated ReactJS and Firebase project structure for the app itself and offered technical support and advice for the team that took the project on full time.",
+      "Provided direct engineering support in the initial phase of the project, scaffolding the integration between the ReactJS frontend and real time communication with Google Firebase",
+      "Offered advise on consultation on website design and the design system implementation.",
     ],
-    tags: ["React", "Google Firebase", "Express", "NodeJS"]
+    tags: ["ReactJS", "Google Firebase", "Express", "NodeJS"]
   },
   {
     id: "2678aa159f464ce6de5b",
@@ -75,9 +78,11 @@ export const freelance: Job[] = [
     range: "",
     jobtitle: "Frontend Engineer",
     description: [
-      "Landing page for a social network app."
+      "Created a landing page for an entrepreneur who wanted to create a social media platform based upon wellness.",
+      "Landing page architected with a simple React frontend with search engine optimization.",
+      "Advised client on design system to meet their branding requirements."
     ],
-    tags: ["React", "TailwindCSS"],
+    tags: ["ReactJS", "TailwindCSS"],
   }
 ];
 
@@ -94,7 +99,7 @@ export const jobs: Job[] = [
       "Leveraged the Jira/Slack APIs to automate task delegation, facilitating the rapid onboarding of volunteers in the final stretch of the campaign.",
       "Scraped county websites to obtain real time, precinct-level vote counts to be ingested by our data pipelines during election night reporting.",
     ],
-    tags: ["Python", "dbt", "Airflow", "GCP", "Bash", "Superblocks", "DNC Portal", "BigQuery", "ETL", "data pipelines", "web scraping"],
+    tags: ["Python", "dbt", "SQL", "Airflow", "GCP", "Hex", "Bash", "shell", "Superblocks", "DNC Portal", "BigQuery", "ETL", "data pipelines", "data orchestration", "web scraping"],
   },
   {
     id: "38993e84-2048-11ed861d-0242ac120002",
@@ -107,7 +112,7 @@ export const jobs: Job[] = [
       "Successfully negotiated the renewal of the organization’s data memorandum with LAUSD by outlining a clear mission and delivery timetable of data products to stakeholders both in the school district and across our departments.",
       "Extended features in our Student Information Systems (PowerSchool) by writing custom plugins in SQL, NodeJS, and Python, automating data delivery to key vendors supporting our schools.",
     ],
-    tags: ["Python", "Node.js", "R", "XML", "Data Cleaning", "ETL", "Salesforce", "Tableau"],
+    tags: ["Python", "SQL", "R", "sftp", "shell", "Bash", "NodeJS", "Google Drive API", "R", "XML", "data cleaning", "ETL", "Salesforce", "Tableau", "PowerSchool",],
   },
   {
     id: "38994532-2048-11ed861d-0242ac120002",
@@ -120,7 +125,7 @@ export const jobs: Job[] = [
       "Conducted analyses in R and Python to perform data matching across different vendor files, increasing the networks' overall reimbursement rate from federal programs.",
       "Led trainings in both the home office and at school sites in the importance of a data-oriented strategy.",
     ],
-    tags: ["Python", "R", "Google Drive API", "Data Cleaning", "ETL", "Tableau"],
+    tags: ["Python", "R", "Google Drive API", "Data Cleaning", "ETL", "Tableau", "PowerSchool",],
   },
 ];
 
