7c56dfdc7a623699bc6897328f7953fdf33cbea2
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-01-28 02:24:58 -0800

    add email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
-import { github, linkedin } from '@/data/socials';
+import { github, linkedin, email, } from '@/data/socials';
 
 export default function Footer() {
   return (
     <div className="flex w-full justify-center py-14">
       <a target="_blank" rel="noopener noreferrer" href={linkedin}><i className={`linkedin link big black icon`}></i></a>
       <a target="_blank" rel="noopener noreferrer" href={github}><i className={`github link big black icon`}></i></a>
+      <a target="_blank" rel="noopener noreferrer" href={email}><i className={`mail link big black icon`}></i></a>
     </div>
   );
 }
--- data/socials.ts
+++ data/socials.ts
@@ -1,2 +1,3 @@
 export const linkedin = 'https://www.linkedin.com/in/christian-a-valenzuela/';
 export const github = 'https://github.com/chris-av';
+export const email = 'mailto:chrisandre95@gmail.com';
