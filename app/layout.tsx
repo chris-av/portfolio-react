993bb72ad5353ad8bbee1ebc933d9a9aea828482
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2025-02-02 22:36:00 -0800

    Update metadata, OpenGraph preview card

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,37 @@ const noto = Nunito({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://chrisav.dev"),
   title: 'Christian Valenzuela',
   description: 'Full Stack Developer, Engineer, Analyst',
+  openGraph: {
+    title: "Christian Valenzuela | Full Stack, Data Engineer",
+    description: [
+      "Experienced Full Stack and Data Engineer specializing in crafting scalable web applications and building robust data workflows.",
+      "Delivering tailored solutions for modern web development and data-driven insights.",
+    ].join(" "),
+    url: "https://yourwebsite.com", 
+    type: "website",
+    locale: "en_US",
+    siteName: "Christian Valenzuela Portfolio", // Your website's name for better branding
+    images: [
+      {
+        url: "/chrisav-og-preview.png",
+        width: 1229,
+        height: 630,
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        width: 512,
+        height: 512,
+      },
+      {
+        url: "/mstile-150x150.png",
+        width: 150,
+        height: 150,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
