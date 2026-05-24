7e3001e4d519157d604d97b7e7441c81a6427655
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2026-05-24 13:01:32 -0700

    add config for images

--- next.config.js
+++ next.config.js
@@ -4,5 +4,23 @@ module.exports = {
   swcMinify: true,
   compiler: {
     styledComponents: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.spotify.com",
+        // port: "",
+        pathname: "/v1/**",
+        // search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        // port: "",
+        pathname: "/image/**",
+        // search: "",
+      },
+    ],
   }
 }
